ddbd39552798627df7d929e49e5126ac4ef96e29
raunak1902 <raunakratan553@gmail.com>
2026-03-18 12:59:27 +0530

    Remove basicSsl from plugins

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     host: true,
     port: 5174,
